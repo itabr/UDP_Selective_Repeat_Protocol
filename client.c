ccc656794cb4046954da2a85c60dbed3ef96f0f9
Stephanie Lam <sytl@yahoo.com>
2017-03-15 01:43:05 -0700

    fix SYN retransmit

--- client.c
+++ client.c
@@ -23,6 +23,20 @@ void error(char *msg) {
     exit(0);
 }
 
+long double get_timestamp()
+{
+  struct timeval timer_usec;
+  long double timestamp_usec; 
+  if (!gettimeofday(&timer_usec, NULL))
+  {
+    timestamp_usec = ((long double)timer_usec.tv_sec) * 100000011 
+    + (long double) timer_usec.tv_usec; 
+  }
+
+  //printf("timestamp = %lld microseconds\n", timestamp_usec); 
+  return timestamp_usec; 
+}
+
 int main(int argc, char **argv) {
     int sockfd, portno, n;
     int serverlen;
@@ -40,8 +54,6 @@ int main(int argc, char **argv) {
     hostname = argv[1];
     portno = atoi(argv[2]);
     filename = argv[3];
-
-    // printf("%s",filename);
 
     /* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -67,36 +79,36 @@ int main(int argc, char **argv) {
 
     /* send the filename to the server */
     serverlen = sizeof(serveraddr); 
+    long double time_sent = get_timestamp(); 
     n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
     if (n < 0) 
       error("ERROR in sendto");
-    
-    /* print the server's reply */
-    //buf[sizeof(filename)] = '\n'; 
 
     struct packet received_packet; 
     bzero(&received_packet, sizeof(struct packet)); 
-
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(sockfd, &readfds);
 
     struct timeval timeout;
     timeout.tv_sec = 0;
     timeout.tv_usec = 500000;
 
-    n = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
-    while (n == 0)
+    int filename_received = 0; 
+    while (!filename_received)
     {
-	printf("Retransmit file name\n");
-        n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
-        if (n < 0) 
-          error("ERROR in sendto");
         fd_set readfds;
         FD_ZERO(&readfds);
-        FD_SET(sockfd, &readfds); 
+        FD_SET(sockfd, &readfds);
         n = select(sockfd + 1, &readfds, NULL, NULL, &timeout); 
-    } 
+        if (n == 0 && get_timestamp() - time_sent > 500000)
+        {
+            printf("Retransmit file name\n"); 
+            time_sent = get_timestamp(); 
+            n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
+            if (n < 0) 
+              error("ERROR in sendto");
+        }
+        else if (n > 0)
+            filename_received = 1; 
+    }
 
     FILE *fp;
 
@@ -106,40 +118,26 @@ int main(int argc, char **argv) {
 
     while (1)
     {   
-	/*
-        if(expected_packet == 30){
-            expected_packet = 0;
-        }
-	*/
-        //bzero(buf, BUFSIZE); 
-        //n = recvfrom(sockfd, buf, sizeof(buf), 0, &serveraddr, &serverlen);
-
         n = recvfrom(sockfd, &received_packet, sizeof(received_packet), 0, &serveraddr, &serverlen); 
         if (n < 0) 
           error("ERROR in recvfrom");
         else if (n > 0) 
         {
-            //if(received_packet.seq_num == expected_packet){
-	    if (received_packet.packet_num == expected_packet){
+    	    if (received_packet.packet_num == expected_packet && received_packet.flag != 3){
                 fwrite(received_packet.data , sizeof(char) , sizeof(received_packet.data) , fp );
                 expected_packet = expected_packet +1;
-		struct node *temp = find(expected_packet);
+		        struct node *temp = find(expected_packet);
 
                 while(temp){
-                    fwrite(temp->new_packet.data , sizeof(char) , 			    sizeof(temp->new_packet.data) , fp );
+                    fwrite(temp->new_packet.data , sizeof(char) , sizeof(temp->new_packet.data) , fp );
                     delete(temp->key);
                     expected_packet = expected_packet + 1;
-			/*
-			if(expected_packet == 30){
-           		 expected_packet = 0;
-        		}
-			*/ 
                     temp = find(expected_packet);
                 }
             }
-            else if(received_packet.packet_num > expected_packet){
-
-                insertFirst(received_packet.packet_num,received_packet);
+            else if(received_packet.packet_num > expected_packet && received_packet.flag != 3){
+                if (find(expected_packet) == NULL)
+                    insertFirst(received_packet.packet_num,received_packet);
 
                 struct node *temp = find(expected_packet);
 
@@ -152,10 +150,60 @@ int main(int argc, char **argv) {
                     temp = find(expected_packet);
                 }
             }
+            /*
+            if (received_packet.packet_num == expected_packet && received_packet.flag == 3) { // received FIN from server
+                // send ACK to server
+                struct packet ack = {1024, received_packet.packet_num % 30, received_packet.packet_num, 0, NULL, 4, get_timestamp()}; // flag = 4 denotes FIN-ACK
+                n = sendto(sockfd, &ack, sizeof(ack), 0, &serveraddr, serverlen); 
+                if (n < 0)
+                    error("ERROR in sendto"); 
 
-            // fwrite(received_packet.data , sizeof(char) , sizeof(received_packet.data) , fp );
+                fd_set readfds;
+                FD_ZERO(&readfds);
+                FD_SET(sockfd, &readfds);
 
-		struct node *temp = find(expected_packet);
+                n = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+                while (n == 0)
+                {
+                    printf("Retransmit FIN-ACK\n");
+                    n = sendto(sockfd, &ack, sizeof(ack), 0, &serveraddr, serverlen); 
+                    if (n < 0) 
+                      error("ERROR in sendto");
+                    fd_set readfds;
+                    FD_ZERO(&readfds);
+                    FD_SET(sockfd, &readfds); 
+                    n = select(sockfd + 1, &readfds, NULL, NULL, &timeout); 
+                } 
+
+                // reaching this point means that server received FIN-ACK
+                // now send FIN 
+                struct packet fin = {1024, (received_packet.packet_num + 1) % 30, received_packet.packet_num + 1, 0, NULL, 3, get_timestamp()}; // flag = 4 denotes FIN-ACK
+                n = sendto(sockfd, &fin, sizeof(fin), 0, &serveraddr, serverlen); 
+                if (n < 0)
+                    error("ERROR in sendto"); 
+
+                fd_set readfds;
+                FD_ZERO(&readfds);
+                FD_SET(sockfd, &readfds);
+
+                n = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+                while (n == 0)
+                {
+                    printf("Retransmit FIN\n");
+                    n = sendto(sockfd, &ack, sizeof(ack), 0, &serveraddr, serverlen); 
+                    if (n < 0) 
+                      error("ERROR in sendto");
+                    fd_set readfds;
+                    FD_ZERO(&readfds);
+                    FD_SET(sockfd, &readfds); 
+                    n = select(sockfd + 1, &readfds, NULL, NULL, &timeout); 
+                } 
+
+
+            }
+            */
+            /*
+		    struct node *temp = find(expected_packet);
 
                 while(temp){
                     fwrite(temp->new_packet.data , sizeof(char) , sizeof(temp->new_packet.data) , fp );
@@ -165,9 +213,10 @@ int main(int argc, char **argv) {
                     expected_packet = expected_packet + 1;
                     temp = find(expected_packet);
                 }
+            */
 
             printf("Receiving packet %d\n", received_packet.packet_num); 
-	    printf("packet nummmmm %d\n", expected_packet);
+	        printf("packet nummmmm %d\n", expected_packet);
 
             int ack_num = received_packet.packet_num;
             printf("ack num = %d\n", ack_num); 
@@ -178,19 +227,7 @@ int main(int argc, char **argv) {
                 error("ERROR in sendto"); 
             printf("Sending ACK #%d\n", received_packet.packet_num); 
         }
-        //bzero(buf, BUFSIZE); 
-
-           // struct packet junk = {1024, 10, "0", 0};
-
-           // insertFirst(1,junk);
-           // insertFirst(2,junk);
-           // insertFirst(3,junk);
-           // insertFirst(4,junk);
-           // insertFirst(5,junk);
-           // insertFirst(6,junk);
-        
-           //print list
-           printList();
+        printList();
     }
 
 
