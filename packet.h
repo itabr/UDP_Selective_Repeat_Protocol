40dafa6f8b1132fb9431e3201f1967610bb296fb
Stephanie Lam <sytl@yahoo.com>
2017-03-15 17:57:06 -0700

    fix data length based on new header fields

--- packet.h
+++ packet.h
@@ -1,13 +1,16 @@
-#define DATA_SIZE 1016
+#define DATA_SIZE 996
 #define CS_SIZE 2
 
 struct packet
 {
-  int len;
+  // fields other than data take up 28 bytes (4 * 5 + 8) 
+  // a packet is 1024 bytes. so th data can be 996 bytes max 
+	
+  int len; 
   int seq_num;
   int packet_num;
   int cs;
   char data[DATA_SIZE];
-  int flag; // 0 = haven't sent, 1 = sent + waiting for ACK, 2 = received ACK 
+  int flag; // 0 = haven't sent, 1 = sent + waiting for ACK, 2 = received ACK, 3 = FIN
   long double timestamp;
 };
